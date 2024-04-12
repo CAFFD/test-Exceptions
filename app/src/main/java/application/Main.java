5adc4660bcb3d4264663d63de838fbe0cdf46165
Gabriel Guarsoni Dadário <81692574+CAFFD@users.noreply.github.com>
2024-04-12 01:35:19 +0000

    PrimeirasExceções

--- app/src/main/java/application/Main.java
+++ app/src/main/java/application/Main.java
@@ -3,6 +3,21 @@ package application;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!!!");
+    
+        int vetor[] = {20, 21, 22, 23};
+
+        try {
+            for(int cont=0 ; cont<=4 ; cont++) {
+                System.out.println(vetor[cont]);
+            }
+            throw new Exception("Forçando a Barra");
+        } catch (ArrayIndexOutOfBoundsException ex) {
+            System.out.println("Indice do vetor Incorreto!");
+        } catch (Exception ex) {
+            System.out.println("Falhou!");
+        } finally {
+            System.out.println("Encerramento!");
+        }
     }
 }
 
